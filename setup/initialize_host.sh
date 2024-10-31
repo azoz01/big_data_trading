5ecd2082379bbf997b2368703bf167d3dc2ab1a8
Antoni Zajko <antoni.zajko.1@gmail.com>
2024-10-31 18:09:17 +0100

    Fix

--- setup/initialize_host.sh
+++ setup/initialize_host.sh
@@ -5,7 +5,13 @@ umask 000
 mkdir -p ../data/hadoop
 mkdir -p ../data/hive
 mkdir -p ../data/elastic
-mkdir -p ../data/nifi
+mkdir -p ../data/nifi/database_repository
+mkdir -p ../data/nifi/flowfile_repository
+mkdir -p ../data/nifi/content_repository
+mkdir -p ../data/nifi/provenance_repository
+mkdir -p ../data/nifi/state
+mkdir -p ../data/nifi/logs
+
 
 sudo chown hadoop:hadoop ../data/hadoop 
 sudo chown hadoop:hadoop ../data/hive
