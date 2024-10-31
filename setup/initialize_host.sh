6eeef445e55aeb4f3e1a2aafb2d85d7575df60ca
Antoni Zajko <antoni.zajko.1@gmail.com>
2024-10-31 17:57:49 +0100

    Fix nifi

--- setup/initialize_host.sh
+++ setup/initialize_host.sh
@@ -5,6 +5,7 @@ umask 000
 mkdir -p ../data/hadoop
 mkdir -p ../data/hive
 mkdir -p ../data/elastic
+mkdir -p ../data/nifi
 
 sudo chown hadoop:hadoop ../data/hadoop 
 sudo chown hadoop:hadoop ../data/hive
