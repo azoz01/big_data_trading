ec9ade6d367a949043a14e284682df307c207616
Antoni Zajko <antoni.zajko.1@gmail.com>
2025-01-20 21:09:02 +0100

    Final fixes

--- code/run_system.sh
+++ code/run_system.sh
@@ -1,11 +1,26 @@
 #!/bin/bash
 
-PYSPARK_PYTHON=./venv/bin/python spark-submit main.py --process online_ml &> online.log &
+PYSPARK_PYTHON=./venv/bin/python spark-submit --master local[*] main.py --process online_ml &> online.log &
 
 
 /bin/bash << EOT &
 while true; do
     PYSPARK_PYTHON=./venv/bin/python spark-submit main.py --process news_to_redis
+    sleep 60
+done
+EOT
+
+
+/bin/bash << EOT &
+while true; do
+    PYSPARK_PYTHON=./venv/bin/python spark-submit main.py --process calculate_roi
+    sleep 60
+done
+EOT
+
+/bin/bash << EOT &
+while true; do
+    PYSPARK_PYTHON=./venv/bin/python spark-submit main.py --process logs_to_elastic
     sleep 60
 done
 EOT
